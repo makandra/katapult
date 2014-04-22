20126a4c1edbcff01a5daac46fa907b1005f2b02
Dominik <dev@tsentraal.de>
2014-04-22 20:28:15 +0200

    Improve Rails app generation step (speedup)

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -1,8 +1,16 @@
 Given /^a pristine Rails application$/ do
   @aruba_timeout_seconds = 20
+  app_name = 'test_app'
+  cache_name = 'cached_test_app'
+  cache_path = File.join('tmp', cache_name)
+  target_dir = File.join('tmp', 'aruba')
 
-  run_simple 'bundle exec rails new test_app --skip-test-unit'
-  assert_passing_with('README')
+  # create cached rails app if missing
+  unless File.directory?(cache_path)
+    command = 'cd tmp; bundle exec rails new #{cache_name} --skip-test-unit'
+    system(command) or raise "Rails app generation failed"
+  end
 
-  cd 'test_app'
+  FileUtils.cp_r cache_path, File.join(target_dir, app_name)
+  cd app_name
 end
