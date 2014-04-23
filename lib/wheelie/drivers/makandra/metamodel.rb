c245274472956c6ac4107fdfc4bcdf443f06021c
Dominik <dev@tsentraal.de>
2014-04-23 17:09:13 +0200

    Tidy up features

--- lib/wheelie/drivers/makandra/metamodel.rb
+++ lib/wheelie/drivers/makandra/metamodel.rb
@@ -4,29 +4,30 @@ module Wheelie
   class Driver
     module Makandra
       class Metamodel
-    
+
         def initialize(path_to_metamodel)
           instance_eval File.read(path_to_metamodel), path_to_metamodel
         end
-    
+
         def render
           @models.each &:render
         end
-    
-        #private
-    
-        def metamodel(name, &block)
-          @name = name
-          @models = []
-          instance_eval(&block)
-        end
-  
+
+        # warum darf diese Methode nicht private sein?
         def model(name, &block)
           model = Model.new(name)
           model.instance_eval(&block) if block_given?
           @models << model
         end
 
+        private
+
+        def metamodel(name, &block)
+          @name = name
+          @models = []
+          instance_eval(&block)
+        end
+        
       end
     end
   end
