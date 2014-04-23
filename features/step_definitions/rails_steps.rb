ac2595b93d91290a7571fa360806d2287fbdd4aa
Dominik <dev@tsentraal.de>
2014-04-23 11:58:22 +0200

    Fix Rails app generation step

--- features/step_definitions/rails_steps.rb
+++ features/step_definitions/rails_steps.rb
@@ -1,16 +1,15 @@
 Given /^a pristine Rails application$/ do
   @aruba_timeout_seconds = 20
-  app_name = 'test_app'
-  cache_name = 'cached_test_app'
+  app_name, cache_name = 'test_app', 'cached_test_app'
+  app_path = File.join('tmp', 'aruba', app_name)
   cache_path = File.join('tmp', cache_name)
-  target_dir = File.join('tmp', 'aruba')
 
   # create cached rails app if missing
   unless File.directory?(cache_path)
-    command = 'cd tmp; bundle exec rails new #{cache_name} --skip-test-unit'
+    command = "cd tmp; bundle exec rails new #{cache_name} --skip-test-unit"
     system(command) or raise "Rails app generation failed"
   end
 
-  FileUtils.cp_r cache_path, File.join(target_dir, app_name)
+  FileUtils.cp_r cache_path, app_path
   cd app_name
 end
