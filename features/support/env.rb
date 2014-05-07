fb58caf2a8911495c62016b09aa7354b1310a03b
Dominik Schöler <dominik.schoeler@makandra.de>
2014-05-07 14:09:54 +0200

    Now really disable Spring in tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,12 @@
 require 'aruba/cucumber'
 require 'pry'
 
-# Spring leads to all kinds of unexpected behavior in tests.
-DISABLE_SPRING = true
+# Use this method to wrap any system calls to the test application
+def prepare_environment(&block)
+  Bundler.with_clean_env do
+    # Spring leads to all kinds of unexpected behavior in tests.
+    ENV['DISABLE_SPRING'] = '1'
+
+    block.call
+  end
+end
