39eb329926bfa7de4c6fab7a04bfa9fa42e5f2e0
Dominik Schöler <dominik.schoeler@makandra.de>
2014-06-05 17:16:47 +0200

    Introduce Reference class that can be queried about all elements in a metamodel

---
+++ lib/wheelie/reference.rb
@@ -0,0 +1,22 @@
+# This class serves as a global reference for elements in a metamodel.
+
+require 'singleton'
+
+module Wheelie
+  class Reference
+    include Singleton
+
+    attr_accessor :metamodel
+
+    def set_metamodel(metamodel)
+      self.metamodel = metamodel
+    end
+
+    def model(name)
+      metamodel.models.find do |model|
+        model.name == name
+      end
+    end
+
+  end
+end
