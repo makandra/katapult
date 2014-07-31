d5b5e591dc3faa13a4a1364266dad3f069e5e0b7
Dominik Schöler <dominik.schoeler@makandra.de>
2014-07-31 15:24:19 +0200

    Fix route naming

--- lib/generators/wheelie/w_u_i/templates/_route.rb
+++ lib/generators/wheelie/w_u_i/templates/_route.rb
@@ -1,5 +1,5 @@
-% rails_actions = wui.actions.select{ |a| WUI::RAILS_ACTIONS.include? a.name }
-resources :<%= plural_name %>, only: <%= rails_actions.map(&:name).map(&:to_sym) %> do
+% rails_actions = wui.actions - wui.custom_actions
+resources <%= model_name(:symbols) %>, only: <%= rails_actions.map(&:name).map(&:to_sym) %> do
     member do
 % wui.custom_actions.select(&:member?).each do |action|
       <%= action.method %> '<%= action.name %>'
