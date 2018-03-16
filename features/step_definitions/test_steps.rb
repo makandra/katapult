8121ce8faa642a310ec9f546d1429d3ae972622e
Dominik Schöler <dominik.schoeler@makandra.de>
2018-03-16 14:21:44 +0100

    Fix parallel_tests incompatibility with Cucumber 2

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -1,9 +1,11 @@
 When /^I run rspec/ do
   run_simple 'rspec', exit_timeout: 10
+  run_simple 'bundle exec parallel_test -t rspec -- spec', exit_timeout: 10
 end
 
 When /^I run cucumber/ do
   run_simple 'bundle exec cucumber', exit_timeout: 15
+  run_simple 'bundle exec parallel_test -t cucumber -- features', exit_timeout: 15
 end
 
 When 'debugger' do
