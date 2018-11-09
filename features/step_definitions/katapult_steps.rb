011f480704856b566192abf50db64351a3884989
Dominik Schöler <dominik.schoeler@makandra.de>
2018-11-09 15:36:03 +0100

    Improve deploy.rb for Webpack

--- features/step_definitions/katapult_steps.rb
+++ features/step_definitions/katapult_steps.rb
@@ -92,7 +92,7 @@ set :linked_files, %w(config/database.yml config/secrets.yml)
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w(log public/system tmp/pids)
+set :linked_dirs, %w(log public/system tmp/pids node_modules public/packs)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
